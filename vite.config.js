039d44fbcd5167cd8a594ef85a5d3fa956080f80
__nik <nik@blackquadrant.com>
2023-08-24 08:57:02 +1000

    Updating configs

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  css: {
+    preprocessorOptions: {
+      less: {
+        javascriptEnabled: true,
+        additionalData: '@root-entry-name: default;',
+      },
+    },
+  },
   plugins: [react()],
+  // https://github.com/aws-amplify/amplify-js/issues/9639 - Cannot use amplify-js in browser environment (breaking vite / snowpack / esbuild)
+  resolve: { alias: { './runtimeConfig': './runtimeConfig.browser' } }
 })

94e4b9821482121c0076ec14e45d249027cfb3e8
__nik <nik@blackquadrant.com>
2023-10-29 08:31:30 +1000

    cert statuses

--- src/view/components/custom/CertStatus.jsx
+++ src/view/components/custom/CertStatus.jsx
@@ -3,10 +3,11 @@ import { isNull } from "lodash";
 import { Link } from "react-router-dom";
 
 export default function CertStatus({ data, record }) {
-    return isNull(record.status) || record.status === 'P' ? <Tag color="gold">Pending Approval</Tag> :
-        record.status === 'A' ? <Tag color="green">Active</Tag> :
-            record.status === 'R' ? <Tag color="red">Rejected</Tag> :
-                record.status === 'E' ? <Tag color="#c00000">Superceded</Tag> :
-                    isNull(record.inspector) ? <Tag color="gold">Pending Approval</Tag> : null;
+    return record.type === 'Self Declaration' ? <Tag color="blue">Self Declaration</Tag> :
+        isNull(record.status) || record.status === 'P' ? <Tag color="gold">Pending Approval</Tag> :
+            record.status === 'A' ? <Tag color="green">Active</Tag> :
+                record.status === 'R' ? <Tag color="orange">Rejected</Tag> :
+                    record.status === 'E' ? <Tag color="#c00000">Superceded</Tag> :
+                        isNull(record.inspector) ? <Tag color="gold">Pending Approval</Tag> : null;
     // return <pre>{JSON.stringify({ data, record }, false, 4)}</pre>
 }
